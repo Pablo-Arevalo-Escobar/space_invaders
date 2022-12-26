e02ab8d2703f1d2a7b5dcb9ce65a462581800ba6
Fescrico <73705641+Fescrico@users.noreply.github.com>
2022-12-26 06:23:23 +0000

    Mobs now move down the screen

--- Mob.gd
+++ Mob.gd
@@ -1,13 +1,31 @@
 extends Area2D
 
 var direction = Vector2(-1.0,0.0)
-var speed = 000.0
-var screen_size
+var speed = 020.0
+var screen_size	
+var ysteps
+var xLimit
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	xLimit = screen_size.x/10
+	ysteps = screen_size.y/15
+	add_to_group("Enemies")
 	
 func _process(delta):
-  position = position + speed * direction * delta
+	var temporaryPos
+	temporaryPos = position + speed * direction * delta
+	if(temporaryPos.x < xLimit or temporaryPos.x > screen_size.x - xLimit):
+		var tree = get_tree()
+		get_tree().call_group("Enemies", "turn_around")
+		temporaryPos =  position
+	position = temporaryPos + speed * direction * delta
+  
+func turn_around():
+	if ($TurnLimit.is_stopped()):
+		$TurnLimit.start()
+		direction.x = direction.x * -1
+		position.y = position.y + ysteps
+	
   
 
